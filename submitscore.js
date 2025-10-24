3b2648338dcb874c79ae97475d7031e9d21e4226
100lumis <lumikukkak@gmail.com>
2025-10-24 08:57:35 +0300

    Delete submitscore.js
    away with you

--- submitscore.js
+++
@@ -1,8 +0,0 @@
-async function submitScore(score, time) {
-  await fetch('http://localhost:3000/submit-score', {
-    method: 'POST',
-    credentials: 'include', 
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ score, time })
-  });
-}
