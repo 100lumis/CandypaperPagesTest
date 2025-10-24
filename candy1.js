b3c0fd45755d67ffe529d3d279d5271a86eafc70
100lumis <lumikukkak@gmail.com>
2025-10-24 09:15:33 +0300

    Update candy1.js

--- candy1.js
+++ candy1.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="stylesheet" href="Candy1.css">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">  
-</head>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  
-  <script>
-    function bounce() {
+ function bounce() {
       $(".fairy")
         .animate({ top: "-=20px" }, 800) 
         .animate({ top: "+=20px" }, 800, bounce); 
@@ -19,9 +9,3 @@
     });
 
 document.getElementById("loginMessage").textContent = "Kirjautuminen onnistui!";
-  </script>
-
-
-
-  </body>
-  </html>
